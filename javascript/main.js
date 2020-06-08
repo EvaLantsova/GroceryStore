a85d08d56723b7afed6fc86462843617a8175be8
EvaLantsova <55274804+EvaLantsova@users.noreply.github.com>
2020-06-08 23:04:10 +0300

    adding main page+styling main page

--- javascript/main.js
+++ javascript/main.js
@@ -4,9 +4,9 @@ let stickerStar = document.querySelector('.sticker_star')
 for (let i = 0; i < 10; i++) {
 	let newStarImg = document.createElement('img')
 	newStarImg.classList.add('star')
-	newStarImg.src = 'img/star1.svg'
+	newStarImg.src = '../img/star1.svg'
 	newStarImg.onclick = function() {
-		newStarImg.setAttribute('src', 'img/star2.svg');
+	newStarImg.setAttribute('src', '../img/star2.svg');
 	}
 	newStarImg.style.transform = `translate(${anime.random(1,100)}vw, ${anime.random(1,100)}vh)`
 	stickerStar.append(newStarImg)
@@ -15,13 +15,13 @@ for (let i = 0; i < 10; i++) {
 let stars = document.querySelectorAll('.star')
 stars.forEach(function(element) {
 	element.onclick = function() {
-		element.setAttribute('src', 'img/star2.svg');
+		element.setAttribute('src', '../img/star2.svg');
 	}
 })
 
 let infiniteLoopAlternate = anime({
   targets: '.pineapple',
-  translateX: 250,
+  transl ateX: 250,
 	translateY: -250,
 	duration: 2000,
   direction: 'alternate',
