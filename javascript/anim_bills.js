f0faca3d1749efa36490a7c56dc41ea7b01a8799
EvaLantsova <55274804+EvaLantsova@users.noreply.github.com>
2020-06-13 00:28:20 +0300

    continue making animation

--- javascript/anim_bills.js
+++ javascript/anim_bills.js
@@ -1,16 +1,38 @@
+// let starsNumber = 10
+// let stickerStar = document.querySelector('.sticker_star')
+//
+// for (let i = 0; i < 10; i++) {
+// 	let newStarImg = document.createElement('img')
+// 	newStarImg.classList.add('star')
+// 	newStarImg.src = '../img/star2.svg'
+// 	newStarImg.onclick = function() {
+// 	newStarImg.setAttribute('src', '../img/star2.svg');
+// 	}
+// 	newStarImg.style.transform = `translate(${anime.random(1,100)}vw, ${anime.random(1,100)}vh)`
+// 	stickerStar.append("newStarImg")
+// }
+
+
 let starsNumber = 10
 let stickerStar = document.querySelector('.sticker_star')
 
 for (let i = 0; i < 10; i++) {
-	let newStarImg = document.createElement('img')
-	newStarImg.classList.add('star')
-	newStarImg.src = '../img/star2.svg'
-	newStarImg.onclick = function() {
-	newStarImg.setAttribute('src', '../img/star2.svg');
-	}
-	newStarImg.style.transform = `translate(${anime.random(1,100)}vw, ${anime.random(1,100)}vh)`
-	stickerStar.append("newStarImg")
+let newStarImg = document.createElement('img')
+newStarImg.classList.add('star')
+newStarImg.src = '../img/star1.svg'
+newStarImg.onclick = function() {
+newStarImg.setAttribute('src', '../img/star1.svg');
 }
+newStarImg.style.transform = `translate(${anime.random(1,100)}vw, ${anime.random(1,100)}vh)`
+stickerStar.append('newStarImg')
+}
+
+let starr = document.querySelectorAll('.star')
+starr.forEach(function(element) {
+element.onclick = function() {
+element.setAttribute('src', '../img/star2.svg');
+}
+})
 
 let stars = document.querySelectorAll('.star')
 stars.forEach(function(element) {
@@ -19,17 +41,17 @@ stars.forEach(function(element) {
 	}
 })
 
-// let infiniteLoopAlternate = anime({
-//   targets: '.pineapple',
-//   translateX: 250,
-// 	translateY: -250,
-// 	duration: 2000,
-//   direction: 'alternate',
-//   loop: true
-// });
+let infiniteLoopAlternate = anime({
+  targets: '.pineapple',
+  translateX: 250,
+	translateY: -250,
+	duration: 2000,
+  direction: 'alternate',
+  loop: true
+});
 
 
-// var image = document.getElementsByClassName('kiwi');
-// new simpleParallax('kiwi', {
-// 	scale: 1.5
-// });
+var image = document.getElementsByClassName('kiwi');
+new simpleParallax('kiwi', {
+	scale: 1.5
+});
