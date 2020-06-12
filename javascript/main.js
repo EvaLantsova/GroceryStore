5925977288c809433fe1bb7873726ba2821e66da
EvaLantsova <55274804+EvaLantsova@users.noreply.github.com>
2020-06-12 17:39:31 +0300

    TRYING TO ADD MODAL WINDOW, ADD LINK ON STCKER, BUT IT DOESNT WORK

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,4 @@
-/*let starsNumber = 10
+let starsNumber = 10
 let stickerStar = document.querySelector('.sticker_star')
 
 for (let i = 0; i < 10; i++) {
@@ -33,41 +33,57 @@ var image = document.getElementsByClassName('kiwi');
 new simpleParallax('kiwi', {
 	scale: 1.5
 });
-*/
 
-
-let btnMakeBananas = document.querySelector('.horizontal_banana_sticker')
-btnMakeBananas.onclick = function() {
-    let numberOfPictures = 100
-    for (let i = 0; i < numberOfBananas; i++) {
-        let imgBanana = document.createElement('img')
-        imgBanana.classList.toggle('imgBanana.png')
-        imgBanana.style.position = 'fixed'
-        imgBanana.style.transform = `translate(${anime.random(0,90)}vw, ${anime.random(0,100)}vh)`
-        document.body.append(imgBanana)
-
-        imgBanana.addEventListener('mouseenter', function() {
-            imgBanana.style.display = 'none';
-        });
-    }
+document.getElementById('click_stick').onclick = function() {
+document.getElementById('click_banana').style.opacity = '100%';
 }
 
-$(document).ready(function(){
 
-$(".mymagicoverbox").click(function()
-{
- $('#myfond_gris').fadeIn(300);
- var iddiv = $(this).attr("iddiv");
- $('#'+iddiv).fadeIn(300);
- $('#myfond_gris').attr('opendiv',iddiv);
- return false;
+
+const btns = document.querySelectorAll('.btn');
+const modalOverlay = document.querySelector('.modal-overlay');
+
+btns.forEach((el) => {
+  el.addEventListener('click',(e) => {
+    let path = e.currentTarget.getAttribute('data-path');
+    console.log('path');
+    document.querySelector(`[data-target="${path}"]`).classList.add('modal--visible');
+    modalOverlay.classList.add('modal-overlay--visible');
+
+  });
 });
 
-$('#myfond_gris, .mymagicoverbox_fermer').click(function()
-{
- var iddiv = $("#myfond_gris").attr('opendiv');
- $('#myfond_gris').fadeOut(300);
- $('#'+iddiv).fadeOut(300);
-});
 
-});
+// let btnMakeBananas = document.querySelector('.horizontal_banana_sticker')
+// btnMakeBananas.onclick = function() {
+//     let numberOfPictures = 100
+//     for (let i = 0; i < numberOfPictures, i++) {
+//         let imgBanana = document.createElement('img')
+//         imgBanana.classList.toggle('imgBanana.png')
+//         imgBanana.style.position = 'fixed'
+//         imgBanana.style.transform = `translate(${anime.random(0,90)}vw, ${anime.random(0,100)}vh)`
+//         document.body.append(imgBanana)
+//
+//         imgBanana.addEventListener('mouseenter', function() {
+//             imgBanana.style.display = 'none';
+//         });
+//     }
+// }
+
+// $(document).ready(function(){
+//
+// $(".mymagicoverbox").click(function()
+// {
+//  $('#myfond_gris').fadeIn(300);
+//  var iddiv = $(this).attr("iddiv");
+//  $('#'+iddiv).fadeIn(300);
+//  $('#myfond_gris').attr('opendiv',iddiv);
+//  return false;
+// });
+//
+// $('#myfond_gris, .mymagicoverbox_fermer').click(function()
+// {
+//  var iddiv = $("#myfond_gris").attr('opendiv');
+//  $('#myfond_gris').fadeOut(300);
+//  $('#'+iddiv).fadeOut(300);
+// });
