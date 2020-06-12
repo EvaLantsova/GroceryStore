4a393e94ecc2ba70de0dde635e73bfe1985f3c6f
EvaLantsova <55274804+EvaLantsova@users.noreply.github.com>
2020-06-12 23:59:05 +0300

    added new js page

--- javascript/main.js
+++ javascript/main.js
@@ -1,38 +1,3 @@
-let starsNumber = 10
-let stickerStar = document.querySelector('.sticker_star')
-
-for (let i = 0; i < 10; i++) {
-	let newStarImg = document.createElement('img')
-	newStarImg.classList.add('star')
-	newStarImg.src = '../img/star2.svg'
-	newStarImg.onclick = function() {
-	newStarImg.setAttribute('src', '../img/star2.svg');
-	}
-	newStarImg.style.transform = `translate(${anime.random(1,100)}vw, ${anime.random(1,100)}vh)`
-	stickerStar.append("newStarImg")
-}
-
-let stars = document.querySelectorAll('.star')
-stars.forEach(function(element) {
-	element.onclick = function() {
-		element.setAttribute('src', '../img/star2.svg');
-	}
-})
-
-let infiniteLoopAlternate = anime({
-  targets: '.pineapple',
-  translateX: 250,
-	translateY: -250,
-	duration: 2000,
-  direction: 'alternate',
-  loop: true
-});
-
-
-var image = document.getElementsByClassName('kiwi');
-new simpleParallax('kiwi', {
-	scale: 1.5
-});
 
 document.getElementById('click_stick').onclick = function() {
 document.getElementById('click_banana').style.opacity = '100%';
